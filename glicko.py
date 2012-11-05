0cae8da9053271df24a9b54f6566498ec315448e
Heungsub Lee <h@subl.ee>
2012-11-06 01:49:24 +0900

    pass Glicko2 test case

--- glicko.py
+++ glicko.py
@@ -9,7 +9,7 @@
     :license: BSD, see LICENSE for more details.
 """
 from datetime import datetime
-from math import log, pi, sqrt
+from math import e, exp, log, pi, sqrt
 from time import mktime, time
 
 
@@ -17,12 +17,15 @@ from time import mktime, time
 WIN = 1.
 #: The actual score for draw
 DRAW = 0.5
-#: The actual score for lose
-LOSE = 0.
+#: The actual score for loss
+LOSS = 0.
 
 
 MU = 1500
 SIGMA = 350
+VOLATILITY = 0.06
+TAU = 1.0
+EPSILON = 0.000001
 Q = log(10) / 400
 
 
@@ -44,6 +47,14 @@ class Rating(object):
             return front + ')'
         else:
             return front + ', rated_at=%r)' % self.rated_at
+
+
+class Rating2(Rating):
+
+    def __init__(self, mu=MU, sigma=SIGMA, volatility=VOLATILITY,
+                 rated_at=None):
+        super(Rating2, self).__init__(mu, sigma, rated_at)
+        self.volatility = volatility
 
 
 class Glicko(object):
@@ -70,6 +81,7 @@ class Glicko(object):
         if rated_at is None:
             rated_at = utctime()
         d_square_inv = 0
+        #: Glickman called it "Delta"
         difference = 0
         for actual_score, other_rating in series:
             g = self.g(other_rating)
@@ -83,6 +95,100 @@ class Glicko(object):
         return self.create_rating(mu, sigma, rated_at)
 
 
+class Glicko2(Glicko):
+
+    def __init__(self, mu=MU, sigma=SIGMA, tau=TAU, epsilon=EPSILON,
+                 period=86400):
+        super(Glicko2, self).__init__(mu, sigma, period)
+        self.tau = tau
+        self.epsilon = epsilon
+
+    def create_rating(self, mu=None, sigma=None, volatility=None,
+                      rated_at=None):
+        if mu is None:
+            mu = self.mu
+        if sigma is None:
+            sigma = self.sigma
+        if volatility is None:
+            volatility = self.volatility
+        return Rating2(mu, sigma, volatility, rated_at)
+
+    scale = 173.7178
+
+    def rescale(self, rating):
+        return self.create_rating((rating.mu - 1500) / self.scale,
+                                  rating.sigma / self.scale, rating.volatility)
+
+    def rescale2(self, rating):
+        return self.create_rating(self.scale * rating.mu + 1500,
+                                  self.scale * rating.sigma, rating.volatility)
+
+    def g(self, rating):
+        return 1 / sqrt(1 + (3 * rating.sigma ** 2) / (pi ** 2))
+
+    def expect_score(self, rating, other_rating, g=1):
+        return 1. / (1 + exp(-g * (rating.mu - other_rating.mu)))
+
+    def determine_volatility(self, rating, delta, variance):
+        sigma = rating.sigma
+        volatility = rating.volatility
+        delta_squared = delta ** 2
+        alpha = log(volatility ** 2)
+        def f(x):
+            return ((exp(x) * (delta_squared - sigma ** 2 - variance - exp(x))) / \
+                   (2 * (sigma ** 2 + variance + exp(x)) ** 2)) - \
+                   ((x - alpha) / (self.tau ** 2))
+        if delta_squared > sigma ** 2 + variance:
+            B = log(delta_squared - sigma ** 2 - variance)
+        else:
+            k = 1
+            while f(alpha - k * sqrt(self.tau ** 2)) < 0:
+                k += 1
+            B = alpha - k * sqrt(self.tau ** 2)
+        A = alpha
+        fa, fb = f(A), f(B)
+        while abs(B - A) > self.epsilon:
+            C = A + (A - B) * fa / (fb - fa)
+            fc = f(C)
+            if fc * fb < 0:
+                A = B
+                fa = fb
+            else:
+                fa /= 2
+            B = C
+            fb = fc
+        return e ** (A / 2)
+
+    def rate(self, rating, series, rated_at=None):
+        if rated_at is None:
+            rated_at = utctime()
+        d_square_inv = 0
+        variance_inv = 0 #g2
+        delta = 0
+        rating = self.rescale(rating)
+        for actual_score, other_rating in series:
+            other_rating = self.rescale(other_rating)
+            g = self.g(other_rating)
+            expected_score = self.expect_score(rating, other_rating, g)
+            g = round(g, 4)
+            expected_score = round(expected_score, 3)
+            variance_inv += g ** 2 * expected_score * (1 - expected_score) #g2
+            delta += g * (actual_score - expected_score)
+            d_square_inv += expected_score * (1 - expected_score) * \
+                            (Q ** 2) * (g ** 2)
+        variance = 1. / variance_inv
+        delta *= variance
+        denom = 1. / (rating.sigma ** 2) + d_square_inv
+        mu = rating.mu + Q / denom * (delta / variance_inv)
+        sigma = sqrt(1 / denom)
+        volatility = self.determine_volatility(rating, delta, variance)
+        sigma_star = sqrt(sigma ** 2 + volatility ** 2)
+        sigma = 1 / sqrt(1 / sigma_star ** 2 + 1 / variance)
+        mu = rating.mu + sigma ** 2 * (delta / variance)
+        return self.rescale2(self.create_rating(mu, sigma, volatility, rated_at))
+
+
+
 def rate_1vs1(rating1, rating2, drawn=False):
     return rate(rating1, [(DRAW if drawn else WIN, rating2)]), \
-           rate(rating2, [(DRAW if drawn else LOSE, rating1)])
+           rate(rating2, [(DRAW if drawn else LOSS, rating1)])
--- glickotests.py
+++ glickotests.py
@@ -7,20 +7,50 @@ from glicko import *
 suite = Tests()
 
 
+class almost(object):
+
+    def __init__(self, val, precision=3):
+        self.val = val
+        self.precision = precision
+
+    def almost_equals(self, val1, val2):
+        if round(val1, self.precision) == round(val2, self.precision):
+            return True
+        fmt = '%.{0}f'.format(self.precision)
+        mantissa = lambda f: int((fmt % f).replace('.', ''))
+        return abs(mantissa(val1) - mantissa(val2)) <= 1
+
+    def __eq__(self, other):
+        try:
+            if not self.almost_equals(self.val.volatility, other.volatility):
+                return False
+        except AttributeError:
+            pass
+        return self.almost_equals(self.val.mu, other.mu) and \
+               self.almost_equals(self.val.sigma, other.sigma)
+
+    def __repr__(self):
+        return repr(self.val)
+
+
 @suite.test
-def glickman_example():
+def glicko_glickman_example():
     env = Glicko()
-    print 53670.85
-    print (0.9955**2)*(0.639)*(1-0.639)
-    print (0.9531**2)*(0.432)*(1-0.432)
-    print (0.7242**2)*(0.303)*(1-0.303)
-    print 1. /((0.0057565 ** 2) * ((0.9955**2)*(0.639)*(1-0.639) + \
-                              (0.9531**2)*(0.432)*(1-0.432) + \
-                              (0.7242**2)*(0.303)*(1-0.303)))
     r1 = Rating(1500, 200)
     r2 = Rating(1400, 30)
     r3 = Rating(1550, 100)
     r4 = Rating(1700, 300)
-    assert env.rate(r1, [(WIN, r2), (LOSE, r3), (LOSE, r4)]) == \
-           Rating(1464.106, 151.399)
-    #assert env.rate(r1, [(WIN, r2), (LOSE, r3), (LOSE, r4)]) == Rating(1464, 151.4)
+    rated = env.rate(r1, [(WIN, r2), (LOSS, r3), (LOSS, r4)])
+    assert almost(rated) == Rating(1464.106, 151.399) # Rating(1464, 151.4)
+
+
+@suite.test
+def glicko2_glickman_example():
+    env = Glicko2(tau=0.5)
+    r1 = Rating2(1500, 200, 0.06)
+    r2 = Rating2(1400, 30)
+    r3 = Rating2(1550, 100)
+    r4 = Rating2(1700, 300)
+    rated = env.rate(r1, [(WIN, r2), (LOSS, r3), (LOSS, r4)])
+    assert almost(rated) == Rating2(1464.086, 151.507, 0.05999)
+    # Rating2(1464.06, 151.52, 0.05999)
